e7d2724a79ee7cb721296a580d36fabbdbdd7e8d
Vlad Tiberiu Mihailescu <vtmihailescu@gmail.com>
2020-04-08 20:50:08 +0400

    Added difference

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -5,127 +5,80 @@
 #include "common.h"
 
 
-void testOpenImage()
-{
+void testGaussianPyr() {
+	return;
+}
+Mat_<Vec3b> difference(Mat_<Vec3b> a, Mat_<Vec3b> b) {
+	Mat_<Vec3b> ret(a.rows, a.cols, 0);
+	if (a.rows != b.rows || a.cols != b.cols) {
+		std::cout << "\nDiferenta intre doua matrici de dimensiuni diferite\n";
+		return ret;
+	}
+	for (int i = 0; i < a.rows; ++i) {
+		for (int j = 0; j < a.cols; ++j) {
+			ret[i][j][0] = a[i][j][0] - b[i][j][0];
+			ret[i][j][1] = a[i][j][1] - b[i][j][1];
+			ret[i][j][2] = a[i][j][2] - b[i][j][2];
+		}
+	}
+	return ret;
+}
+std::vector<Mat_<Vec3b> > generateLaplacianPyr(Mat_<Vec3b> inputImage, int layers) {
+	std::vector<Mat_<Vec3b> > ret;
+	//std::vector<Mat_<Vec3b> > gaussianPyr = generateGaussianPyr(inputImage, layers);
+
+	for (int i = 0; i < layers; ++i) {
+		Mat_<Vec3b> test(255, 255, { 0, 0, 0 });
+
+		ret.push_back(test);
+	}
+
+	return ret;
+}
+void testLaplacianPyr() {
 	char fname[MAX_PATH];
-	while (openFileDlg(fname))
-	{
+	while (openFileDlg(fname)) {
 		Mat src;
-		src = imread(fname);
+		src = imread(fname, CV_LOAD_IMAGE_COLOR);
+
+		int layers;
+		std::cout << "\nDati numarul de layere\n";
+		std::cin >> layers;
+
+
+		std::vector<Mat_<Vec3b> > laplacianPyr = generateLaplacianPyr(src, layers);
+
+		for (int i = 0; i < laplacianPyr.size(); ++i) {
+			std::string layerName = "laplace pyr #";
+			layerName += std::to_string(i);
+			imshow(layerName, laplacianPyr[i]);
+		}
+
 		imshow("image", src);
 		waitKey();
 	}
+	return;
 }
-
-void testOpenImagesFld()
-{
-	char folderName[MAX_PATH];
-	if (openFolderDlg(folderName) == 0)
-		return;
-	char fname[MAX_PATH];
-	FileGetter fg(folderName, "bmp");
-	while (fg.getNextAbsFile(fname))
-	{
-		Mat src;
-		src = imread(fname);
-		imshow(fg.getFoundFileName(), src);
-		if (waitKey() == 27) //ESC pressed
-			break;
-	}
-}
-
-void testImageOpenAndSave()
-{
-	Mat src, dst;
-
-	src = imread("Images/Lena_24bits.bmp", CV_LOAD_IMAGE_COLOR);	// Read the image
-
-	if (!src.data)	// Check for invalid input
-	{
-		printf("Could not open or find the image\n");
-		return;
-	}
-
-	// Get the image resolution
-	Size src_size = Size(src.cols, src.rows);
-
-	// Display window
-	const char* WIN_SRC = "Src"; //window for the source image
-	namedWindow(WIN_SRC, CV_WINDOW_AUTOSIZE);
-	cvMoveWindow(WIN_SRC, 0, 0);
-
-	const char* WIN_DST = "Dst"; //window for the destination (processed) image
-	namedWindow(WIN_DST, CV_WINDOW_AUTOSIZE);
-	cvMoveWindow(WIN_DST, src_size.width + 10, 0);
-
-	cvtColor(src, dst, CV_BGR2GRAY); //converts the source image to a grayscale one
-
-	imwrite("Images/Lena_24bits_gray.bmp", dst); //writes the destination to file
-
-	imshow(WIN_SRC, src);
-	imshow(WIN_DST, dst);
-
-	printf("Press any key to continue ...\n");
-	waitKey(0);
-}
-
-void testNegativeImage()
-{
-	char fname[MAX_PATH];
-	int MAX_GREY_VALUE = 256;
-	while (openFileDlg(fname))
-	{
-		Mat_<uchar> src = imread(fname, CV_LOAD_IMAGE_GRAYSCALE);
-		int height = src.rows;
-		int width = src.cols;
-		Mat_<uchar> dst = Mat_<uchar>(height, width);
-		// Asa se acceseaaza pixelii individuali pt. o imagine cu 8 biti/pixel
-		for (int i = 0; i < height; i++)
-		{
-			for (int j = 0; j < width; j++)
-			{
-				uchar val = src(i, j);
-				uchar neg = MAX_GREY_VALUE - val;
-				dst(i, j) = neg;
-			}
-		}
-		imshow("Input image", src);
-		imshow("Negative image", dst);
-		waitKey();
-	}
-}
-
-void new_function() {
-	printf("Bau Bau!");
-	printf("Bau! Bau! Ca trebuie!");
-}
-
-int main()
-{
+int main() {
 	int op;
 	do
 	{
 		system("cls");
 		destroyAllWindows();
 		printf("Menu:\n");
-		printf(" 1 - Open image\n");
-		printf(" 2 - Open BMP images from folder\n");
-		printf(" 3 - Negative image\n");
-		printf(" 0 - Exit\n\n");
+		printf(" 1 - Generate Gaussian Pyramid\n");
+		printf(" 2 - Generate Laplacian Pyramid\n");
+		printf(" 0 - \n\n");
 		printf("Option: ");
 		scanf("%d", &op);
 		switch (op)
 		{
 		case 1:
-			testOpenImage();
+			testGaussianPyr();
 			break;
 		case 2:
-			testOpenImagesFld();
+			testLaplacianPyr();
 			break;
-		case 3:
-			testNegativeImage();
-			break;
-
 		}
 	} while (op != 0);
 	return 0;
